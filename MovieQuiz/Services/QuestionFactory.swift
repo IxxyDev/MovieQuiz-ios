7022be2cea2165b3de9dfca8d9eda52cadedda4d
IxxyDev <denisenkoforwork@gmail.com>
2023-11-22 17:23:05 +0300

    task ready

--- MovieQuiz/Services/QuestionFactory.swift
+++ MovieQuiz/Services/QuestionFactory.swift
@@ -1,32 +1,68 @@
 import Foundation
 
-class QuestionFactory: QuestionFactoryProtocol {
-    weak var delegate: QuestionsFactoryDelegate?
-    private let questionLabel = "Рейтинг этого фильма больше чем 5?"
-    private lazy var questions: [QuizQuestion] = [
-        QuizQuestion(image: "The Green Elephant", text: questionLabel, isCorrectAnswer: true),
-        QuizQuestion(image: "Pyat butylok vodki", text: questionLabel, isCorrectAnswer: true),
-        QuizQuestion(image: "Vase de noces", text: questionLabel, isCorrectAnswer: false),
-        QuizQuestion(image: "Srpski film", text: questionLabel, isCorrectAnswer: true),
-        QuizQuestion(image: "Neposredstvenno Kakha", text: questionLabel, isCorrectAnswer: false),
-        QuizQuestion(image: "The Best Movie", text: questionLabel, isCorrectAnswer: false),
-        QuizQuestion(image: "Lords of the Lockerroom", text: questionLabel, isCorrectAnswer: true),
-        QuizQuestion(image: "The Room", text: questionLabel, isCorrectAnswer: false),
-        QuizQuestion(image: "Schastlivyy konets", text: questionLabel, isCorrectAnswer: false),
-        QuizQuestion(image: "Sharknado", text: questionLabel, isCorrectAnswer: false),
-    ]
+final class QuestionFactory: QuestionFactoryProtocol {
+    private let moviesLoader: MoviesLoading
+    private weak var delegate: QuestionsFactoryDelegate?
+//    private lazy var questions: [QuizQuestion] = [
+//        QuizQuestion(image: "The Green Elephant", text: questionLabel, isCorrectAnswer: true),
+//        QuizQuestion(image: "Pyat butylok vodki", text: questionLabel, isCorrectAnswer: true),
+//        QuizQuestion(image: "Vase de noces", text: questionLabel, isCorrectAnswer: false),
+//        QuizQuestion(image: "Srpski film", text: questionLabel, isCorrectAnswer: true),
+//        QuizQuestion(image: "Neposredstvenno Kakha", text: questionLabel, isCorrectAnswer: false),
+//        QuizQuestion(image: "The Best Movie", text: questionLabel, isCorrectAnswer: false),
+//        QuizQuestion(image: "Lords of the Lockerroom", text: questionLabel, isCorrectAnswer: true),
+//        QuizQuestion(image: "The Room", text: questionLabel, isCorrectAnswer: false),
+//        QuizQuestion(image: "Schastlivyy konets", text: questionLabel, isCorrectAnswer: false),
+//        QuizQuestion(image: "Sharknado", text: questionLabel, isCorrectAnswer: false),
+//    ]
+    private var movies: [MostPopularMovie] = []
     
-    init(delegate: QuestionsFactoryDelegate? = nil) {
+    init(moviesLoader: MoviesLoading, delegate: QuestionsFactoryDelegate? = nil) {
+        self.moviesLoader = moviesLoader
         self.delegate = delegate
     }
     
-    func requestNextQuestion() {
-        guard let index = (0..<questions.count).randomElement() else {
-            delegate?.didReceiveNextQuestion(question: nil)
-            return
+    func loadData() {
+        moviesLoader.loadMovies { [weak self] result in
+            DispatchQueue.main.async {
+                guard let self = self else { return }
+                switch result {
+                case .success(let mostPopularMovies):
+                    self.movies = mostPopularMovies.items
+                    self.delegate?.didLoadDataFromServer()
+                case .failure(let error):
+                    self.delegate?.didFailToLoadData(with: error)
+                }
+            }
         }
-        
-        let question = questions[safe: index]
-        delegate?.didReceiveNextQuestion(question: question)
+    }
+    
+    func requestNextQuestion() {
+        DispatchQueue.global().async { [weak self] in
+            guard let self = self else { return }
+            let idx = (0..<self.movies.count).randomElement() ?? 0
+            
+            guard let movie = self.movies[safe: idx] else { return }
+            
+            var imageData = Data()
+            
+            do {
+                imageData = try Data(contentsOf: movie.resizedImageURL)
+            } catch {
+                print("Failed to load image")
+            }
+            
+            let rating = Float(movie.rating) ?? 0
+            let ratingForQuestion: Float = [rating - 1, rating + 1].randomElement() ?? 0.0
+            let text = "Рейтинг этого фильма больше чем \(Int(ratingForQuestion))?"
+            let correctAnswer = rating > ratingForQuestion
+            
+            let question = QuizQuestion(image: imageData, text: text, isCorrectAnswer: correctAnswer)
+            
+            DispatchQueue.main.async { [weak self] in
+                guard let self = self else { return }
+                self.delegate?.didReceiveNextQuestion(question: question)
+            }
+        }
     }
 }
--- MovieQuiz/Services/QuestionsFactoryDelegate.swift
+++ MovieQuiz/Services/QuestionsFactoryDelegate.swift
@@ -2,4 +2,6 @@ import Foundation
 
 protocol QuestionsFactoryDelegate: AnyObject {
     func didReceiveNextQuestion(question: QuizQuestion?)
+    func didLoadDataFromServer()
+    func didFailToLoadData(with error: Error)
 }
